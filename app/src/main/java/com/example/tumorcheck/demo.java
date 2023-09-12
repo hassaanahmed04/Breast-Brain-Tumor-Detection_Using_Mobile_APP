16e8145519894d00d5f52e1081280d10485eeda3
Hassaaan Ahmed <61899518+hassaanahmed04@users.noreply.github.com>
2023-09-13 00:08:24 +0500

    Update demo.java

--- app/src/main/java/com/example/tumorcheck/demo.java
+++ app/src/main/java/com/example/tumorcheck/demo.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class saqlain extends AppCompatActivity {
+public class demo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saqlain);
+        setContentView(R.layout.activity_demo);
     }
 }
